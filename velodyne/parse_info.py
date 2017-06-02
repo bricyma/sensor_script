26af585b8e1002043c55161bd0780e48eeea1b0e
bricyma <zhibei.ma@tusimple.ai>
2017-06-02 16:47:48 -0700

    update parse_info.py to plot the rotation per packet

--- velodyne/parse_info.py
+++ velodyne/parse_info.py
@@ -17,21 +17,15 @@ diff = []
 def read_data():
     num = 0
     for x in bag.read_messages(topics=['/rosout']):
-        total = x[1]
-        msg = total.msg
-        l = msg.split(' ')
-        # print l[0], l[1], l[2]
-        if num > 100:
-            if float(l[0])>=171 or int(l[0])==0:
-                print l[0], l[1]
-        if num > 40000:
-            index.append(float(l[0]))
-            rotation.append(float(l[1]))
-            if float(l[2]) > 0:
-                diff.append(float(l[2]))
+        total_msg = x[1]
         num += 1
-        if num > 60000:
-            break
+        if num % 10000 != 0 or total_msg.name.strip() != '/velodyne_nodelet_manager':
+            continue
+        msg = total_msg.msg
+
+        l = msg.split(' ')
+        if float(l[2]) > 0:
+            diff.append(float(l[2]))
     bag.close()
 
 
