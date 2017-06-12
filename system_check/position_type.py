dc3676a3c75b3a36a8c464a47790f26b8285c8c8
bricyma <zhibei.ma@tusimple.ai>
2017-06-12 11:24:58 -0700

    add some features in position_type.py

--- system_check/position_type.py
+++ system_check/position_type.py
@@ -13,25 +13,34 @@ class PosType:
         self.bag = rosbag.Bag(bagname)
         self.postype = []
         self.posacc = [] # position accuracy
+        self.satellite_used = [] # satellites used in solution
+        self.satellite = []
 
     def readbag(self):
         for topic, msg, t in self.bag.read_messages(topics=['/novatel_data/bestpos']):
             self.posacc.append(m.sqrt(msg.latitude_std ** 2 + msg.longitude_std ** 2))
             self.postype.append(msg.position_type)
+            self.satellite.append(msg.sol_svs)
         self.bag.close()
 
     def plot(self):
-        plt.subplot(211)
+        plt.subplot(311)
         plt.plot(self.postype, 'r')
         plt.title("Position Type")
 
-        plt.subplot(212)
+        plt.subplot(312)
         plt.plot(self.posacc, 'r')
         plt.title("Position Accuracy")
+
+        plt.subplot(313)
+        plt.plot(self.satellite, 'r')
+        plt.title("Satellites used in solution")
+
         plt.show()
 
 if __name__ == '__main__':
     pos = PosType()
     pos.readbag()
     pos.plot()
+    print np.mean(pos.satellite)
 
