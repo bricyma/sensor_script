a106766fbe28165b49b2d29415356ccf3afcea98
bricyma <zhibei.ma@tusimple.ai>
2017-06-27 15:13:59 -0700

    update postion_type.py

--- system_check/position_type.py
+++ system_check/position_type.py
@@ -47,13 +47,13 @@ class PosType:
             for topic, msg, t in bag.read_messages(topics=['/novatel_data/inspvax']):
                 self.index[count] += 1
                 if count == 0:
-                    if self.index[0] > 15000:
+                    if self.index[0] > 0:
                         self.posacc1.append(m.sqrt(msg.latitude_std ** 2 + msg.longitude_std ** 2))
                         self.postype1.append(msg.position_type)
                         self.altitude_std1.append(msg.altitude_std)
                         self.azimuth_std1.append(msg.azimuth_std)
                 else:
-                    if self.index[1] > 15000:
+                    if self.index[1] > 0:
                         self.posacc2.append(m.sqrt(msg.latitude_std ** 2 + msg.longitude_std ** 2))
                         self.postype2.append(msg.position_type)
                         self.altitude_std2.append(msg.altitude_std)
@@ -71,26 +71,26 @@ class PosType:
 
         plt.subplot(512)
         plt.plot(self.posacc1, 'r', label='bag1 2d position accuracy')
-        plt.plot(self.posacc2, 'b', label='bag2 2d position accuracy')
-        # plt.plot([np.mean(self.posacc1)] * len(self.posacc1), 'r', linestyle='-', linewidth=3, label='average')
+        # plt.plot(self.posacc2, 'b', label='bag2 2d position accuracy')
+        plt.plot([np.mean(self.posacc1)] * len(self.posacc1), 'b', linestyle='-', linewidth=3, label='average')
         # plt.plot([np.mean(self.posacc2)] * len(self.posacc2), 'r', linestyle='-', linewidth=3, label='average')
         plt.title("INSPVAX 2d Horizontal Position Accuracy")
         plt.legend(loc='upper left')
         plt.subplot(513)
         plt.plot(self.altitude_std1, 'r', label='bag1 vertical position accuracy')
-        plt.plot(self.altitude_std2, 'b', label='bag2 vertical position accuracy')
+        # plt.plot(self.altitude_std2, 'b', label='bag2 vertical position accuracy')
         plt.legend(loc='upper left')
         plt.title("INSPVAX Vertical Position Accuracy")
 
         plt.subplot(514)
         plt.plot(self.satellite1, 'r', label='bag1 satellites')
-        plt.plot(self.satellite2, 'b', label='bag2 satellites')
+        # plt.plot(self.satellite2, 'b', label='bag2 satellites')
         plt.legend(loc='upper left')
         plt.title("Satellites used in solution")
 
         plt.subplot(515)
         plt.plot(self.azimuth_std1, 'r', label='bag1 azimuth without velodyne and camera')
-        plt.plot(self.altitude_std2, 'b', label='bag2 azimuth with velodyne and camera')
+        # plt.plot(self.altitude_std2, 'b', label='bag2 azimuth with velodyne and camera')
         plt.legend(loc='upper left')
         plt.xlabel('stamp')
         plt.ylabel('degree')
@@ -102,10 +102,10 @@ class PosType:
 if __name__ == '__main__':
     pos = PosType()
     pos.readbag(0)
-    print 'bag1: average satellites', np.mean(pos.satellite1)
-    print 'bag1: average horizontal position accuracy', np.mean(pos.posacc1)
-    print 'bag1: standard deviation of 2d position accuracy', np.std(pos.posacc1)
-    print 'bag1: average vertical position accuracy', np.mean(pos.altitude_std1)
+    print 'bag1: average satellites', round(np.mean(pos.satellite1),2)
+    print 'bag1: average horizontal position accuracy', round(np.mean(pos.posacc1),2)
+    print 'bag1: standard deviation of 2d position accuracy', round(np.std(pos.posacc1),2)
+    print 'bag1: average vertical position accuracy', round(np.mean(pos.altitude_std1),2)
 
     print 'bag2: average satellites', np.mean(pos.satellite2)
     print 'bag2: average horizontal position accuracy', np.mean(pos.posacc2)
