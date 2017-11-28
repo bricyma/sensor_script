e888193fefba426b5f68a03cd6e712d7d7d236d3
bricyma <zhibei.ma@tusimple.ai>
2017-11-28 13:07:27 +0800

    update tsmap compare

--- map/tsmap_compare.py
+++ map/tsmap_compare.py
@@ -3,30 +3,56 @@ from tsmap import TSMap, Point3d
 import rospy
 from llh2enu.llh2enu_gps_transformer import *
 import matplotlib.pyplot as plt
-
+import struct
 
 class Compare:
-    def __init__(self):
-        self.map_file = "/home/zhibei/Downloads/Beijing_Wuhudao_Forward_20171122_WHD.hdmap"
-        with open(self.map_file, 'rb') as f:
+    def __init__(self, id, map_file):
+        with open(map_file, 'rb') as f:
             submap = f.read()
         self.map_handler = TSMap(submap)
-        self.left_bounds, self.right_bounds = [], []
-
-        self.old_map = {}
-        self.old_map['x'] = []
-        self.old_map['y'] = []
+        # get the start point
+        f = open(map_file, 'rb')
+        f.read(12)
+        version_num = struct.unpack('i', f.read(4))[0]
+        msg = f.read(72)
+        result = struct.unpack('ddddddddd', msg)
+        self.start_point = result[:3]
+        self.base_latlon = struct.unpack('dd', f.read(16))
+        self.id = id
+        self.map = {}
+        self.map['x'] = []
+        self.map['y'] = []
 
     def _get_bounds(self):
         print self.map_handler.bounds[0].points[0]
         for bound in self.map_handler.bounds:
             for point in bound.points:
-                self.old_map['x'].append(point.x)
-                self.old_map['y'].append(point.y)
-        plt.plot(self.old_map['x'], self.old_map['y'], 'ro')
-        plt.show()
+                self.map['x'].append(point.x)
+                self.map['y'].append(point.y)
+        self.map['x'] = np.array(self.map['x'])
+        self.map['y'] = np.array(self.map['y'])
+        self.map['x'] = self.map['x'] - self.start_point[0]
+        self.map['y'] = self.map['y'] - self.start_point[1]
+
+        plt.xlim([-3500, 500])
+        plt.ylim([-3500, 500])
+
+        if self.id == 0:
+            plt.plot(self.map['x'], self.map['y'], 'ro')
+        elif self.id == 1:
+            plt.plot(self.map['x'], self.map['y'], 'bo')
+        else:
+            plt.plot(self.map['x'], self.map['y'], 'go')
+
 
 if __name__ == '__main__':
-    m = Compare()
-    m._get_bounds()
-
+    m_whd = Compare(
+        0, "/home/zhibei/Downloads/Beijing_Wuhudao_Forward_20171122_WHD.hdmap")
+    m_whd._get_bounds()
+    m_cfd = Compare(
+        1, "/home/zhibei/Downloads/Beijing_Wuhudao_Forward_20171122_CFD.hdmap")
+    m_cfd._get_bounds()
+    m_cfd = Compare(
+        2, "/home/zhibei/.octopus/maps/Wuhudao_forward_0912.hdmap")
+    m_cfd._get_bounds()
+    plt.show()
