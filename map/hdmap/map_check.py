3b0bc1f3875d76fc0909c4c52efa6c930f0e6fd3
bricyma <zhibei.ma@tusimple.ai>
2018-03-17 22:53:36 -0700

    fix the map offset problem, due to the llh2enu transformation

--- map/hdmap/map_check.py
+++ map/hdmap/map_check.py
@@ -12,10 +12,11 @@ data = {'test': {'x': [], 'y': [], 'azimuth': [], 'yaw': []},
 
 baseLat = 32.75707
 baseLon = -111.55757
+trans = gps_transformer()
+
 with open(map_file, 'rb') as f:
     submap = f.read()
 map = TSMap(submap)
-trans = gps_transformer()
 
 def parse():
     count = 0
@@ -24,19 +25,20 @@ def parse():
             try:
                 count += 1
                 a = line.split(' ')
-                # x, y = latlon2xy(float(a[0]), float(a[1]))
-                x, y = trans.llh2enu_1(float(a[0]), float(a[1]), 0, baseLat, baseLon, 0)
-
+                x, y = latlon2xy(float(a[0]), float(a[1]))
+                # x, y = trans.llh2enu_2(float(a[0]), float(a[1]), 0, baseLat, baseLon, 0)
                 p = Point3d(x, y)
                 ref_p = map.get_ref_pt(p)
                 data['test']['azimuth'].append(float(a[2]))
-                data['test']['x'].append(float(a[0]))
-                data['test']['y'].append(float(a[1]))
+                data['test']['x'].append(x)
+                data['test']['y'].append(y)
                 data['map']['x'].append(ref_p.x)
                 data['map']['y'].append(ref_p.y)
             except:
                 pass
 
+def RAD2DEG(rad):
+    return rad * 180 / np.pi
 
 def plot():
     for d in data:
@@ -49,30 +51,40 @@ def plot():
         y2 = data[k]['y'][1:]
         y = data[k]['y'][:-1]
         if k == 'test':
-            data[k]['yaw'] = np.arctan2(y2 - y, x2 - x) * 180 / np.pi
+            data[k]['yaw'] = RAD2DEG(np.arctan2(x2 - x, y2 - y))
         else:
-            data[k]['yaw'] = np.arctan2(x2 - x, y2 - y) * 180 / np.pi
+            data[k]['yaw'] = RAD2DEG(np.arctan2(x2 - x, y2 - y))
         for i in range(0, len(data[k]['yaw'])):
             if data[k]['yaw'][i] < 0:
                 data[k]['yaw'][i] += 360
         data[k]['yaw'] = np.append(data[k]['yaw'], data[k]['yaw'][-1])
 
-    plt.subplot(311)
+    plt.subplot(411)
     plt.plot(data['map']['yaw'], 'r', label='map_gps')
     plt.plot(data['test']['yaw'], 'b', label='test_gps')
     plt.plot(data['test']['azimuth'], 'g', label='test_azimuth')
     plt.legend(loc='upper left')
 
-    plt.subplot(312)
-    diff = data['map']['yaw'] - data['test']['yaw']
+    plt.subplot(412)
+    diff = data['map']['yaw'] - data['test']['azimuth']
     diff[abs(diff) > 2] = 0
-    plt.plot(diff, 'r.', label='map - test')
+    plt.plot(diff, 'r.', label='map - test_azimuth')
     plt.legend(loc='upper left')
 
-    plt.subplot(313)
-    plt.plot(data['map']['x'], data['map']['y'], 'r', label='map')
-    plt.plot(data['test']['x'], data['test']['y'], 'r', label='test')
+    plt.subplot(413)
+    diff = data['map']['yaw'] - data['test']['yaw']
+    diff[abs(diff) > 2] = 0
+    plt.plot(diff, 'r.', label='map - test_gps')
     plt.legend(loc='upper left')
+
+    plt.subplot(414)
+    diff = data['test']['yaw'] - data['test']['azimuth']
+    diff[abs(diff) > 2] = 0
+    plt.plot(diff, 'r.', label='test_gps - test_azimuth')
+    plt.legend(loc='upper left')
+
+
+
 
     plt.show()
 
