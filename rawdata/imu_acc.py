c0ba72b8f01ddabb30f2a9788fc562ea85fdb094
bricyma <zhibei.ma@tusimple.ai>
2018-04-09 15:57:47 -0700

    check rawimu and corrimudata from novatel

--- rawdata/imu_acc.py
+++ rawdata/imu_acc.py
@@ -1,112 +1,69 @@
 import rosbag
-import math as m
 import matplotlib.pyplot as plt
 import sys
 import numpy as np
 
-bagname = '../../rosbag/'
+bagname = ''
 bagname += sys.argv[1]
 bag = rosbag.Bag(bagname)
-sequence = 0
-index = []
 
-# yr = yaw rate
-ins_yr = []
-vehicle_yr = []
-vehicle_imu_yr = []
+ins = {'acc_x': [], 'acc_y': [], 'acc_z': [],
+       'ang_x': [], 'ang_y': [], 'ang_z': []}
+imu = {'acc_x': [], 'acc_y': [], 'acc_z': [],
+       'ang_x': [], 'ang_y': [], 'ang_z': []}
 
-ins_angle_x = []
-vehicle_imu_angle_x = []
-ins_angle_y = []
-vehicle_imu_angle_y = []
-
-ins_acc_x = []
-ins_acc_y = []
-ins_acc_z = []
-
-imu_acc_x = []
-imu_acc_y = []
-imu_acc_z = []
-
-veh_acc_x = []
-veh_acc_y = []
-veh_acc_z = []
-
-veh_vel_xx = []
-veh_vel = []
-veh_vel_acc = []
-
-imu = []
-ins = []
-veh = []
+# IMU: IMU-IGM-S1
 gyro_scale_factor = 2**-21
-# gyro_scale_factor = 0.1/(3600.0*256.0)
 acc_scale_factor = 2**-22
-# acc_scale_factor = 0.05/(2**15)
 imu_rate = 125
 
 
 def read_data():
     for topic, msg, t in bag.read_messages(topics=['/novatel_data/rawimu']):
-        imu_acc_x.append(msg.x_accel*acc_scale_factor*imu_rate)
-        imu_acc_y.append(-msg.y_accel*acc_scale_factor*imu_rate)
-        imu_acc_z.append(msg.z_accel*acc_scale_factor*imu_rate)
+        imu['acc_x'].append(msg.x_accel * acc_scale_factor * imu_rate)
+        imu['acc_y'].append(-msg.y_accel * acc_scale_factor * imu_rate)
+        imu['acc_z'].append(msg.z_accel * acc_scale_factor * imu_rate)
+        imu['ang_x'].append(msg.pitch_rate * gyro_scale_factor)
+        imu['ang_y'].append(-msg.roll_rate * gyro_scale_factor)
+        imu['ang_z'].append(msg.yaw_rate * gyro_scale_factor)
 
     for topic, msg, t in bag.read_messages(topics=['/novatel_data/corrimudata']):
-        ins_acc_x.append(msg.x_accel*imu_rate)
-        ins_acc_y.append(msg.y_accel*imu_rate)
-        ins_acc_z.append(msg.z_accel*imu_rate)
-
-    flag = 1
-    for topic, msg, t in bag.read_messages(topics=['/vehicle/imu/data_raw']):
-        if flag:
-            flag = 0
-            veh_acc_x.append(msg.linear_acceleration.x)
-            veh_acc_y.append(msg.linear_acceleration.y)
-            veh_acc_z.append(msg.linear_acceleration.z)
-        else:
-            flag = 1
-
-    before = 0
-    for topic, msg, t in bag.read_messages(topics=['/vehicle/twist']):
-        veh_vel.append(msg.twist.linear.x)
-        now = msg.twist.linear.x
-        x_acc = (now-before)/0.02
-        veh_vel_acc.append(x_acc)
-        before = now
-
+        ins['acc_x'].append(msg.x_accel * imu_rate)
+        ins['acc_y'].append(msg.y_accel * imu_rate)
+        ins['acc_z'].append(msg.z_accel * imu_rate)
+        ins['ang_x'].append(msg.pitch_rate * imu_rate)
+        ins['ang_y'].append(msg.roll_rate * imu_rate)
+        ins['ang_z'].append(msg.yaw_rate * imu_rate)
     bag.close()
-
-    imu.append(imu_acc_x)
-    imu.append(imu_acc_y)
-    imu.append(imu_acc_z)
-    ins.append(ins_acc_x)
-    ins.append(ins_acc_y)
-    ins.append(ins_acc_z)
-    veh.append(veh_acc_x)
-    veh.append(veh_acc_y)
-    veh.append(veh_acc_z)
 
 
 def plot():
-    for i in range(0, 3):
-        # plt.plot(veh_vel_acc[::2], 'r', label='veh acc from twist')
-        plt.plot(ins[i], 'b', label='corrimudata')
-        plt.plot(imu[i], 'g', label='rawimu')
-        if i == 0:
-            plt.plot(veh[i], 'r', label='negative vehicle imu')
-            plt.title("Linear acceleration on X-axle between CORRIMUDATA and RAWIMU")
-        if i == 1:
-            plt.plot(veh[i], 'r', label='vehicle imu')
-            plt.title("Linear acceleration on Y-axle between CORRIMUDATA and RAWIMU")
-        if i == 2:
-            plt.plot(veh[i], 'r', label='vehicle imu')
-            plt.title("Linear acceleration on Z-axle between CORRIMUDATA and RAWIMU")
-        plt.legend(loc='upper left')
-        plt.xlabel('stamp')
-        plt.ylabel('linear acceleration (m/s^2)')
-        plt.show()
+    plt.subplot(311)
+    plt.plot(imu['ang_x'], 'r', label='raw imu ang_x')
+    plt.plot(ins['ang_x'], 'g', label='corrected ang_x')
+    plt.legend(loc='upper left')
+    plt.subplot(312)
+    plt.plot(imu['ang_y'], 'r', label='raw imu ang_y')
+    plt.plot(ins['ang_y'], 'g', label='corrected ang_y')
+    plt.legend(loc='upper left')
+    plt.subplot(313)
+    plt.plot(imu['ang_z'], 'r', label='raw imu ang_z')
+    plt.plot(ins['ang_z'], 'g', label='corrected ang_z')
+    plt.legend(loc='upper left')
+    plt.show()
 
-
+    plt.subplot(311)
+    plt.plot(imu['acc_x'], 'r', label='raw imu acc_x')
+    plt.plot(ins['acc_x'], 'g', label='corrected acc_x')
+    plt.legend(loc='upper left')
+    plt.subplot(312)
+    plt.plot(imu['acc_y'], 'r', label='raw imu acc_y')
+    plt.plot(ins['acc_y'], 'g', label='corrected acc_y')
+    plt.legend(loc='upper left')
+    plt.subplot(313)
+    plt.plot(imu['acc_z'], 'r', label='raw imu acc_z')
+    plt.plot(ins['acc_z'], 'g', label='corrected acc_z')
+    plt.legend(loc='upper left')
+    plt.show()
 read_data()
 plot()
